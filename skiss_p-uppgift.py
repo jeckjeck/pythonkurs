fff01a434142c1e3c06fa07b382120464a8281be
Joakim Bäcklund <you@example.com>
2017-06-22 02:48:44 +0200

    more instructions

--- skiss_p-uppgift.py
+++ skiss_p-uppgift.py
@@ -13,6 +13,20 @@ class Animal:
         with open ('animal_list.txt', 'a') as f:
             f.writelines(self.name + '\n' + str(self.age) + '\n' + self.art + '\n' + self.gender + '\n')
 
+
+Animal('Bebbe', 5, 'Gorilla', 'Hona').save_animal()
+
+
+Animal('Bollen',2, 'Panda', 'Hane').save_animal()
+Animal('Cosmos',7, 'Surikat', 'Hane').save_animal()
+Animal('Diddi',3, 'Alligator', 'Hane').save_animal()
+Animal('Skutt',4, 'Antilop', 'Hona').save_animal()
+Animal('Wingo',12, 'Alligator', 'Hona').save_animal()
+Animal('Marcello',6, 'Elg', 'Hane').save_animal()
+Animal('Flax',15, 'Papegoja', 'Hane').save_animal()
+Animal('Sipper',12, 'Gnu', 'Hona').save_animal()
+Animal('BowWow',7, 'Antilop', 'Hane').save_animal()
+
 class Zoo:
     animals = []
     def add_animal(self, ani):
@@ -28,31 +42,21 @@ class Zoo:
             print(animal)
 
 
-
-Animal('Bebbe', 5, 'Gorilla', 'Hona').save_animal()
-Animal('Bollen',2, 'Panda', 'Hane').save_animal()
-Animal('Cosmos',7, 'Surikat', 'Hane').save_animal()
-Animal('Diddi',3, 'Alligator', 'Hane').save_animal()
-Animal('Skutt',4, 'Antilop', 'Hona').save_animal()
-Animal('Wingo',12, 'Alligator', 'Hona').save_animal()
-Animal('Marcello',6, 'Elg', 'Hane').save_animal()
-Animal('Flax',15, 'Papegoja', 'Hane').save_animal()
-Animal('Sipper',12, 'Gnu', 'Hona').save_animal()
-
-
-
-animal_1 = {'name': 'Bebbe', 'age':  5, 'art': 'Gorilla', 'gender': 'Hona'}
-animal_2 = {'name': 'Bollen', 'age': 2, 'art': 'Panda', 'gender': 'Hane'}
-animal_3 = {'name': 'Cosmos', 'age': 7, 'art': 'Surikat', 'gender': 'Hane'}
-animal_4 = {'name': 'Diddi','age': 3, 'art':' Alligator', 'gender': 'Hane'}
-animal_5 = {'name': 'Skutt', 'age': 4, 'art': 'Antilop', 'gender': 'Hona'}
-animal_6 = {'name': 'Wingo', 'age': 12, 'art': 'Alligator', 'gender':'Hona'}
-animal_7 = {'name': 'Marcello','age': 6, 'art': 'Elk', 'gender': 'Hane'}
-animal_8 = {'name': 'Flax', 'age': 15, 'art': 'Papegoja', 'gender': 'Hane'}
-animal_9 = {'name': 'Sipper','age': 12, 'art': 'Gnu', 'gender': 'Hona'}
-
-animal_list = [animal_1, animal_2, animal_3, animal_4, animal_5, animal_6, animal_7, animal_8, animal_9]
-
+# animal_1 = {'name': 'Bebbe', 'age':  5, 'art': 'Gorilla', 'gender': 'Hona'}
+# animal_2 = {'name': 'Bollen', 'age': 2, 'art': 'Panda', 'gender': 'Hane'}
+# animal_3 = {'name': 'Cosmos', 'age': 7, 'art': 'Surikat', 'gender': 'Hane'}
+# animal_4 = {'name': 'Diddi','age': 3, 'art':' Alligator', 'gender': 'Hane'}
+# animal_5 = {'name': 'Skutt', 'age': 4, 'art': 'Antilop', 'gender': 'Hona'}
+# animal_6 = {'name': 'Wingo', 'age': 12, 'art': 'Alligator', 'gender':'Hona'}
+# animal_7 = {'name': 'Marcello','age': 6, 'art': 'Elk', 'gender': 'Hane'}
+# animal_8 = {'name': 'Flax', 'age': 15, 'art': 'Papegoja', 'gender': 'Hane'}
+# animal_9 = {'name': 'Sipper','age': 12, 'art': 'Gnu', 'gender': 'Hona'}
+#
+#
+# animal_1 = {'name': 'Bebbe', 'age':  5, 'art': 'Gorilla', 'gender': 'Hona'}
+# animal_2 = {'name': 'Bollen', 'age': 2, 'art': 'Panda', 'gender': 'Hane'}
+# animal_list = [animal_1, animal_2, animal_3, animal_4, animal_5, animal_6, animal_7, animal_8, animal_9]
+#
 
 
 
